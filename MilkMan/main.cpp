5e31ce765707cf7e96d14b56607a96cdd947abe5
Akshay Kumar Gupta <akshaygupta1994@outlook.com>
2015-08-07 23:53:42 +0530

    changes

--- MilkMan/main.cpp
+++ MilkMan/main.cpp
@@ -11,19 +11,28 @@ int main()
    for(int i=0;i<n;i++)
    {
      cin>>a[i];
-
    }
-  int count=0;
+  int count=0;int count2=0;
    for(int i=0;i<n;i++)
    {
-       int temp;
-    count = a[i]/10;
-    a[i] = a[i]%10;
-    count += a[i]/7;
-    a[i] = a[i]%7;
-    count += a[i]/5;
-    a[i] = a[i]%5;
-    count += a[i]/1;
-    cout<<count<<endl;
+    int temp;int temp2;
+    temp2=a[i];
+    count = temp2/10;
+    temp2 = temp2%10;
+    count += temp2/7;
+    temp2 = temp2%7;
+    count += temp2/5;
+    temp2 = temp2%5;
+    count += temp2/1;
+    temp2=a[i];
+    count2 = temp2/7;
+    temp2 = temp2%7;
+    count2 += temp2/5;
+    temp2 = temp2%5;
+    count2 += temp2/1;
+    if(count2>count)
+        cout<<count<<endl;
+    else
+        cout<<count2<<endl;
    }
 }
--- catch-22/main.cpp
+++ catch-22/main.cpp
@@ -5,81 +5,91 @@ using namespace std;
 int main()
 {
 
-	int *f, *b, *t, *fd, *bd, n, tem, tem2;
+	int *f, *b, *t, *fd, *bd, n, tem;
 
-	cin>>n;
-	f= new int[n];
-	b= new int[n];
-	t= new int[n];
-	fd= new int[n];
-	bd=new int[n];
-	cout<<"hello";
+	cin >> n;
+	f = new int[n];
+	b = new int[n];
+	t = new int[n];
+	fd = new int[n];
+	bd = new int[n];
+
 	for (int i = 0; i<n; i++)
 	{
-		cin>>f[i];
-		cin>>b[i];
-		cin>>t[i];
-		cin>>fd[i];
-		cin>>bd[i];
+		cin >> f[i];
+		cin >> b[i];
+		cin >> t[i];
+		cin >> fd[i];
+		cin >> bd[i];
 	}
-
 	for (int i = 0; i<n; i++)
 	{
+		tem = 0;
 		if (f[i] == b[i])
 		{
 
 			if (f[i] >= fd[i])
-				cout<<fd[i]*t[i]<<" F";
-
-
+				cout << fd[i] * t[i] << " F"<<endl;
 			else
 			{
-				cout<<"No Ditch";
-
+				cout << "No Ditch "<<endl ;
 			}
 		}
 		else
-        {
+		{
 			if (f[i]>b[i])
 			{
-				fd[i] = fd[i] - f[i];
-				tem = f[i] - b[i];
-				tem2 = fd[i] / tem;
-				fd[i] = fd[i]%tem;
-				if (fd[i] != 0)
+				if (f[i]>=fd[i])
 				{
-					tem2++;
-					fd[i] = fd[i] - tem;
-				}
-				fd[i] = fd[i] + f[i];
-				tem = f[i] + b[i];
-				int lol = (fd[i]*t[i]);
-				tem2 = tem2*tem*t[i];
-				tem2 = tem2 + lol ;
+					cout << fd[i] * t[i] << " F "<<endl ;
 
-				cout<<tem2<<" F";
+				}
+				else {
+					while (true)
+					{
+						fd[i] = fd[i] - f[i];fd[i] = fd[i] + b[i];
+						tem += (f[i] + b[i])*t[i];
+						if (fd[i]<=f[i])
+						{
+							tem += fd[i] * t[i];
+							cout << tem << " F "<<endl;
+							break;
+						}
+
+					}
+				}
+
+
 			}
 			else if (b[i]>f[i])
 			{
-
-				tem = b[i] - f[i];
-				tem2 = bd[i] / tem;
-				bd[i] = bd[i]%tem;
-				if (bd != 0)
+				if (bd[i]<=(b[i] - f[i]))
+				{
+					tem += bd[i] * t[i];
+					tem += 2 * f[i] * t[i];
+					cout << tem << " B "<<endl;
+				}
+				else
 				{
 
-					bd[i] = bd[i] +f[i]+f[i];
+
+					while (true)
+					{
+						bd[i] = bd[i] + f[i] - b[i];
+						tem += (f[i] + b[i])*t[i];
+						if (bd[i]<=(b[i] - f[i]))
+						{
+							tem += bd[i] * t[i];
+							tem += 2 * f[i] * t[i];
+							cout << tem << " B "<<endl;
+							break;
+						}
+					}
+
 				}
-
-				tem = f[i] + b[i];
-
-				tem2 = tem2*tem*t[i];
-				tem2 = tem2 + (bd[i]*t[i]);
-
-				cout<<tem2<<" B";
 			}
 		}
 
 	}
-    return 0;
+	return 0;
 }
