177558bccf9cc7df46bff0b53f69ca5444e0d10d
Akshay Kumar Gupta <akshaygupta1994@outlook.com>
2015-08-07 21:28:49 +0530

    new cathch 22

--- catch-22/main.cpp
+++ catch-22/main.cpp
@@ -1,66 +1,82 @@
-#include <stdio.h>
+#include <iostream>
 
 using namespace std;
 
 int main()
 {
 
-	int f, b, t, fd, bd, n, tem, tem2;
-	scanf("%d",&n);
+	int *f, *b, *t, *fd, *bd, n, tem, tem2;
+
+	cin>>n;
+	f= new int[n];
+	b= new int[n];
+	t= new int[n];
+	fd= new int[n];
+	bd=new int[n];
+	cout<<"hello";
 	for (int i = 0; i<n; i++)
 	{
-		scanf("%d", &f); scanf("%d", &b); scanf("%d", &t); scanf("%d", &fd); scanf("%d", &bd);
-		if (f == b)
+		cin>>f[i];
+		cin>>b[i];
+		cin>>t[i];
+		cin>>fd[i];
+		cin>>bd[i];
+	}
+
+	for (int i = 0; i<n; i++)
+	{
+		if (f[i] == b[i])
 		{
 
-			if (f >= fd)
-				printf("%d F ", fd*t);
+			if (f[i] >= fd[i])
+				cout<<fd[i]*t[i]<<" F";
 
 
 			else
 			{
-				printf("No Ditch ");
+				cout<<"No Ditch";
 
 			}
 		}
-		else {
-			if (f>b)
+		else
+        {
+			if (f[i]>b[i])
 			{
-				fd = fd - f;
-				tem = f - b;
-				tem2 = fd / tem;
-				fd = fd%tem;
-				if (fd != 0)
+				fd[i] = fd[i] - f[i];
+				tem = f[i] - b[i];
+				tem2 = fd[i] / tem;
+				fd[i] = fd[i]%tem;
+				if (fd[i] != 0)
 				{
 					tem2++;
-					fd = fd - tem;
+					fd[i] = fd[i] - tem;
 				}
-				fd = fd + f;
-				tem = f + b;
-				int lol = (fd*t);
-				tem2 = tem2*tem*t;
+				fd[i] = fd[i] + f[i];
+				tem = f[i] + b[i];
+				int lol = (fd[i]*t[i]);
+				tem2 = tem2*tem*t[i];
 				tem2 = tem2 + lol ;
 
-				printf("%d F ", tem2);
+				cout<<tem2<<" F";
 			}
-			else if (b>f)
+			else if (b[i]>f[i])
 			{
 
-				tem = b - f;
-				tem2 = bd / tem;
-				bd = bd%tem;
+				tem = b[i] - f[i];
+				tem2 = bd[i] / tem;
+				bd[i] = bd[i]%tem;
 				if (bd != 0)
 				{
 
-					bd = bd +f+f;
+					bd[i] = bd[i] +f[i]+f[i];
 				}
 
-				tem = f + b;
+				tem = f[i] + b[i];
 
-				tem2 = tem2*tem*t;
-				tem2 = tem2 + (bd*t);
+				tem2 = tem2*tem*t[i];
+				tem2 = tem2 + (bd[i]*t[i]);
 
-				printf("%d B ", tem2);
+				cout<<tem2<<" B";
 			}
 		}
 
